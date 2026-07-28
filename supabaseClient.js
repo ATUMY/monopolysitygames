58860f0340a2317e08994905dace29fa9c961f4d
ATUMY <44136035+ATUMY@users.noreply.github.com>
2026-07-28 18:29:18 +0300

    supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://gqnjkonerjhjpqlblwxp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxbmprb25lcmpoanBxbGJsd3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDMxODIsImV4cCI6MjEwMDgxOTE4Mn0.c-Gbre-nxl_zxYUvGeSCXAI3DVMdB1IWxC1SX0k5r2Y';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
